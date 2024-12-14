cd0f0f659d610bc0068af5105764e89e049cae37
Samantha <samanthavillafana8@gmail.com>
2024-12-14 14:39:04 -0500

    create my routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Patients from "./Patients";
+import Doctors from "./Doctors";
+import Appointments from "./Appointments";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <>
+      <NavBar />
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/patients" component={Patients} />
+          <Route path="/doctors" component={Doctors} />
+          <Route path="/appointments" component={Appointments} />
+        </Switch>
+      </div>
+    </>
+  );
 }
 
 export default App;
