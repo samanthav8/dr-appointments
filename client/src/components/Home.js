cad0ae236f1650699dfa69438d86027b1b75cb32
Samantha <samanthavillafana8@gmail.com>
2024-12-14 19:41:23 -0500

    navbar and home styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Home() {
-  return <h1>Welcome to the Project Client</h1>;
+  return (
+    <div className="home-container">
+      <div className="banner">
+        <div className="banner-text">
+          <h1>Welcome to the Appointment Portal</h1>
+          <p>Book and manage your appointments with your favorite doctors.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,19 +3,48 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav >
-      <NavLink to="/">
-        Home
-      </NavLink>
-      <NavLink to="/patients">
-        Patients
-      </NavLink>
-      <NavLink to="/doctors">
-        Doctors
-      </NavLink>
-      <NavLink to="/appointments">
-        Appointments
-      </NavLink>
+    <nav className="navbar" style={{ backgroundColor: '#dff1fb' }}>
+      <div className="container-fluid">
+        <ul className="navbar-nav d-flex flex-row justify-content-center w-100">
+          <li className="nav-item">
+            <NavLink 
+              exact 
+              to="/" 
+              className="nav-link" 
+              activeClassName="active"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink 
+              to="/patients" 
+              className="nav-link" 
+              activeClassName="active"
+            >
+              Patients
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink 
+              to="/doctors" 
+              className="nav-link" 
+              activeClassName="active"
+            >
+              Doctors
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink 
+              to="/appointments" 
+              className="nav-link" 
+              activeClassName="active"
+            >
+              Appointments
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
